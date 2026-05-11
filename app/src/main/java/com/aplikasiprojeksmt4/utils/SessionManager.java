c21b5fcd87ca21bac4a28720337fb66e8b680cfd
FindoRZH_ <Findorizhanta05@gmail.com>
2026-05-11 11:49:19 +0700

    updated

---
+++ app/src/main/java/com/aplikasiprojeksmt4/utils/SessionManager.java
@@ -0,0 +1,32 @@
+package com.aplikasiprojeksmt4.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SessionManager {
+    private static final String PREF_NAME = "UserSession";
+    private static final String KEY_USERNAME = "username";
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private Context context;
+
+    public SessionManager(Context context) {
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    public void saveUsername(String username) {
+        editor.putString(KEY_USERNAME, username);
+        editor.apply();
+    }
+
+    public String getUsername() {
+        return pref.getString(KEY_USERNAME, "User");
+    }
+
+    public void logout() {
+        editor.clear();
+        editor.apply();
+    }
+}
