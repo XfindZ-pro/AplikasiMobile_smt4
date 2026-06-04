c92e0f389f9f239c83d8c03bf016a1b56f74d56b
FindoRZH_ <Findorizhanta05@gmail.com>
2026-06-04 14:35:59 +0700

    updated

--- app/src/main/java/com/aplikasiprojeksmt4/adapters/ProgramAdapter.java
+++ app/src/main/java/com/aplikasiprojeksmt4/adapters/ProgramAdapter.java
@@ -5,8 +5,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.aplikasiprojeksmt4.R;
 import com.aplikasiprojeksmt4.databinding.ItemProgramAdminBinding;
 import com.aplikasiprojeksmt4.models.Program;
+import com.github.bumptech.glide.Glide;
 import java.util.List;
 
 public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramViewHolder> {
@@ -29,6 +31,7 @@ public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramV
     public void onBindViewHolder(@NonNull ProgramViewHolder holder, int position) {
         Program program = programs.get(position);
         holder.binding.tvNamaProgram.setText(program.getNama());
+        holder.binding.tvWilayah.setText(program.getWilayah());
         
         long target = program.getTargetValue();
         long terkumpul = program.getTerkumpul();
@@ -38,14 +41,27 @@ public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ProgramV
         }
         
         holder.binding.progressIndicator.setProgress(progress);
-        holder.binding.tvPersentase.setText(progress + "%");
-        holder.binding.tvTerkumpul.setText("Terkumpul: Rp " + terkumpul);
-        holder.binding.tvTarget.setText("Target: " + program.getTarget());
+        holder.binding.tvTerkumpul.setText("Rp " + String.format("%,d", terkumpul).replace(',', '.') + " terkumpul");
+        holder.binding.tvPersentaseTarget.setText(progress + "% dari target");
+
+        if (program.getImageUrl() != null && !program.getImageUrl().isEmpty()) {
+            Glide.with(holder.itemView.getContext())
+                    .load(program.getImageUrl())
+                    .placeholder(R.drawable.group_2)
+                    .into(holder.binding.ivProgramImage);
+        } else {
+            holder.binding.ivProgramImage.setImageResource(R.drawable.group_2);
+        }
     }
 
     @Override
     public int getItemCount() {
         return programs.size();
+    }
+
+    public void setPrograms(List<Program> newPrograms) {
+        this.programs = newPrograms;
+        notifyDataSetChanged();
     }
 
     public static class ProgramViewHolder extends RecyclerView.ViewHolder {
